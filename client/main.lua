20e38e8730841591167fd646994804d0f25283c3
kirklogan <kirk@ttvav.com>
2019-04-21 19:26:43 -0400

    Test message

--- client/main.lua
+++ client/main.lua
@@ -17,3 +17,7 @@ RegisterNetEvent('esx:setJob')
 AddEventHandler('esx:setJob', function(job)
   PlayerData.job = job
 end)
+
+RegisterCommand("help", function(source)
+    TriggerClientEvent("chatMessage", -1, tostring(GetPlayerName(source) .. " asked for help. But fuck that guy."))
+end)
