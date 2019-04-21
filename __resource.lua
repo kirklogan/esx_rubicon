7b5b6ddbfc16529b366303b549e060eb10720fd7
kirklogan <kirk@ttvav.com>
2019-04-21 19:37:16 -0400

    Fix more stupid

--- __resource.lua
+++ __resource.lua
@@ -3,9 +3,9 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'Rubicon Custom Scripts'
 
 server_scripts {
-	'server.lua'
+	'server/main.lua'
 }
 
 client_scripts {
-	'client.lua'
+	'client/main.lua'
 }
