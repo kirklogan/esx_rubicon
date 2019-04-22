4f9d45972259ec1b2bcea4117bb8addca9c1c4b6
kirklogan <kirk@ttvav.com>
2019-04-22 18:39:20 -0400

    Testing multiline message

--- client/client.lua
+++ client/client.lua
@@ -1,7 +1,7 @@
 RegisterCommand("help", function(source, args)
     TriggerEvent('chat:addMessage', {
 	  color = { 255, 0, 0},
-	  multiline = false,
-	  args = {"Me", "Fuck you!"}
+	  multiline = true,
+	  args = {"[System]", "Fuck you! \n And your mother!"}
 	})
 end, false)
