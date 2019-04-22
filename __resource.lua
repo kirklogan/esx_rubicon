18a0d9c312a498efd4b85a71b8870744ae679640
kirklogan <kirk@ttvav.com>
2019-04-22 18:12:04 -0400

    Cleaned up directory structure and removed some unused files

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,20 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-
 description 'Rubicon Custom Scripts'
 
-client_script 'client.lua'
-server_script 'server.lua'
+ui_page 'html/index.html'
+
+client_scripts {
+    'client/client.lua',
+    'client/killingerr-test.lua'
+} 
+
+server_scripts {
+	'server/server.lua'
+}
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/reset.css',
+    'html/listener.js'
+}
