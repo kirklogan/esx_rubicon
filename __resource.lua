40962844443aaa372bb900964d6eb3bbbe92b676
kirklogan <kirk@ttvav.com>
2019-04-22 19:29:11 -0400

    Another test

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 description 'Rubicon Custom Scripts'
 
-ui_page 'html/Index.html'
+ui_page 'html/index.html'
 
 client_scripts {
 	'client/killingerr-test.lua',
