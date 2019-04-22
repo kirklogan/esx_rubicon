d9d17b635115869edda20d495639f75146fbe576
kirklogan <kirk@ttvav.com>
2019-04-22 18:41:37 -0400

    Testing real help message

--- client/client.lua
+++ client/client.lua
@@ -2,6 +2,6 @@ RegisterCommand("help", function(source, args)
     TriggerEvent('chat:addMessage', {
 	  color = { 255, 0, 0},
 	  multiline = true,
-	  args = {"[System]", "Fuck you! \n And your mother!"}
+	  args = {"HelpBot", "Here are some useful commands:\nPress F2 for inventory\nPress F6 for job related abilities\nPress F1 for your phone"}
 	})
 end, false)
