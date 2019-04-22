8242c8e4f70a8f90c5bb7f61751b46d3537df426
kirklogan <kirk@ttvav.com>
2019-04-22 18:55:47 -0400

    Another test

--- client/client.lua
+++ client/client.lua
@@ -1,21 +1,7 @@
-local display = false
-
 RegisterCommand("help", function(source, args)
     TriggerEvent('chat:addMessage', {
 	  color = { 255, 0, 0},
 	  multiline = true,
 	  args = {"HelpBot", "Here are some useful commands:\nPress F2 for inventory\nPress F6 for job related abilities\nPress F1 for your phone"}
 	})
-end, false)
-
-RegisterCommand("on", function()
-    Citizen.CreateThread(function()
-        TriggerEvent("nui:on", true)
-    end)
-end, false)
-
-RegisterCommand("off", function()
-    Citizen.CreateThread(function()
-        TriggerEvent("nui:off", true)
-    end)
 end, false)
--- client/killingerr-test.lua
+++ client/killingerr-test.lua
@@ -1,7 +1,21 @@
+local display = false
+
 RegisterCommand("testMeDaddy", function(source, args)
     TriggerEvent('chat:addMessage', {
 	  color = { 255, 0, 0},
 	  multiline = false,
 	  args = {"Daddy", "Testing you"}
 	})
+end, false)
+
+RegisterCommand("on", function()
+    Citizen.CreateThread(function()
+        TriggerEvent("nui:on", true)
+    end)
+end, false)
+
+RegisterCommand("off", function()
+    Citizen.CreateThread(function()
+        TriggerEvent("nui:off", true)
+    end)
 end, false)
