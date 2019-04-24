d5e0fb7ba4f846c43c722887ee7a7873cc89d9a5
kirklogan <kirk@ttvav.com>
2019-04-24 18:22:33 -0400

    Another test

--- client/help-menu.lua
+++ client/help-menu.lua
@@ -4,10 +4,8 @@ local showDialog = false
 RegisterCommand("help", function()
     Citizen.CreateThread(function()
         if showDialog == true then
-			showDialog = false
 			TriggerEvent("helpMenu:off")
 		else
-			showDialog = true
 			TriggerEvent("helpMenu:on")
 		end
     end)
@@ -20,6 +18,7 @@ RegisterNetEvent("helpMenu:off")
 
 AddEventHandler("helpMenu:on", function(value)
 	SetNuiFocus(true,true)
+	showDialog = true
     SendNUIMessage({
         showDialog = true
     })
@@ -27,6 +26,7 @@ end)
 
 AddEventHandler("helpMenu:off", function(value)
 	SetNuiFocus(false)
+	showDialog = false
     SendNUIMessage({
         showDialog = false
     })
