68d0ae04c8d9aaa13123c1cdc564c425b2fd6e21
kirklogan <kirk@ttvav.com>
2019-04-26 21:13:04 -0400

    Another test

--- html/listener.js
+++ html/listener.js
@@ -1,5 +1,5 @@
 $(window).ready(function () {
-	$("#tablet").hide();
+	$("#tablet").show();
 	
 	window.addEventListener('message', function (event) {
 		var playerData = event.data.playerData;
@@ -9,7 +9,7 @@ $(window).ready(function () {
 			$("#job").html(playerData.job.label);
 			$("#rank").html('');
 			$("#salary").html('');
-			$("#debug").html(JSON.stringify(playerData, null, 2));
+			$("#debug").html(JSON.stringify(playerData));
 			$("#tablet").show();
 		} else {
 			$("#tablet").hide();
