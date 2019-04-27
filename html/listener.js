33e1bbe20528e6d59f4ae51c9b3108e2ba17ee44
killingerr <rosella.tony@gmail.com>
2019-04-27 15:07:54 -0600

    Testing new features

--- html/listener.js
+++ html/listener.js
@@ -19,7 +19,7 @@ function nuiEventListener() {
             if (event.data['showDialog']) {
                 if (playerData['job']['label'] === 'Police') {
                     $("#rankBtnPolice").show();
-                } else if (playerData['job']['label' === 'EMS']) {
+                } else if (playerData['job']['label'] === 'EMS') {
                     $("#rankBtnEmt").show();
                 }
                 else {
@@ -30,8 +30,8 @@ function nuiEventListener() {
                 $("#salary").html(playerData['job']['grade_salary']);
                 $("#job").html(playerData['job']['label']);
                 $("#rank").html(playerData['job']['grade_label']);
-                //$("#debug").html('');
-                $("#debug").html(JSON.stringify(playerData));
+                $("#debug").html('');
+                //$("#debug").html(JSON.stringify(playerData));
                 $("#tablet").show();
             } else {
                 $("#tablet").hide();
